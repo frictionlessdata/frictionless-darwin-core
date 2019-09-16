0285ec3bb6c6a60c37de3ab2e9b993ef13705b7e
André Heughebaert <a.heughebaert@biodiversity.be>
2019-09-16 15:02:42 +0200

    Pretty datapackage.json in data folder

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='FrictionlessDarwinCore',
     version='0.1',
     author='André Heughebaert',
+    author_email='andrejjh@gmail.com',
     license='MIT License',
     description='A tool converting Darwin Core Archive into Frictionless Data Package.',
     packages=find_packages(),
@@ -16,4 +17,10 @@ setup(
         [console_scripts]
         fdwca=FrictionlessDarwinCore.fdwca:cli
     ''',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
 )
