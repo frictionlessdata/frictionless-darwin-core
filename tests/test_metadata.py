5a30154863cf1e79a2e180f45ef9abf7a0b82415
André Heughebaert <a.heughebaert@biodiversity.be>
2019-09-20 14:58:57 +0200

    Fixing issue#11 Pangeae DarwinCore failure

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -4,9 +4,23 @@ from FrictionlessDarwinCore import DwCMetadata
 
 class TestMetadata(unittest.TestCase):
     S0eml = '../data/S0/eml.xml'
-    S0readme = '../data/S0/readme.md'
-    S0url = 'http://ipt.ala.org.au/eml.do?r=global'
-    S0hd = '35fd4823b18f3520828fcfe957381455'
+    S0hd = '3e95b95a887b3fb7b5019b6daa4dfd2b'
+    C1eml = '../data/C1/eml.xml'
+    C1hd = '7c15bff57d4b6f3811865b3ff5743792'
+    T1eml = '../data/T1/eml.xml'
+    T1hd = 'af947a7d455fc8f4c1cab163d901de2a'
+
+    def test_C1eml(self):
+        """
+        Test that COeml can be opened and documented with expected secure hash and content
+        """
+        f = open(TestMetadata.C1eml)
+        m = DwCMetadata(f.read())
+        f.close()
+        self.assertIsNotNone(m)
+        m.convert()
+        self.assertTrue(m.valid)
+        self.assertEqual(m.hexdigest, TestMetadata.C1hd)
 
     def test_SOeml(self):
         """
@@ -17,7 +31,21 @@ class TestMetadata(unittest.TestCase):
         f.close()
         self.assertIsNotNone(m)
         m.convert()
+        self.assertTrue(m.valid)
         self.assertEqual(m.hexdigest, TestMetadata.S0hd)
+
+    def test_T1eml(self):
+        """
+        Test that T1eml can be opened and documented with expected secure hash and content
+        """
+        f = open(TestMetadata.T1eml)
+        eml = f.read()
+        f.close()
+        m = DwCMetadata(eml)
+        self.assertIsNotNone(m)
+        m.convert()
+        self.assertTrue(m.valid)
+        self.assertEqual(m.hexdigest, TestMetadata.T1hd)
 
 if __name__ == '__main__':
     unittest.main()
